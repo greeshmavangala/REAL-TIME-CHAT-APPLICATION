786b184643e27952b210bc9a7915a2924f5dccb1
greeshmavangala <130574587+greeshmavangala@users.noreply.github.com>
2025-05-11 15:33:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('New client connected:', socket.id);
+
+  socket.on('send_message', (data) => {
+    io.emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+server.listen(5000, () => console.log('Server running on port 5000'));
